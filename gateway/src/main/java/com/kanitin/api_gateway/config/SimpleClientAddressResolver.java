8ce1c690d600080b678a0c62a119d77fa4519e27
Kanitin <kanitin.kr@gmail.com>
2024-02-18 21:34:30 +0700

    feat: implement jwt on gateway

---
+++ gateway/src/main/java/com/kanitin/api_gateway/config/SimpleClientAddressResolver.java
@@ -0,0 +1,14 @@
+package com.kanitin.api_gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component("SimpleClientAddressResolver")
+public class SimpleClientAddressResolver implements KeyResolver {
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just("1");
+    }
+}
--- gateway/src/main/java/com/kanitin/api_gateway/filter/CustomerGlobalFilter.java
+++ gateway/src/main/java/com/kanitin/api_gateway/filter/CustomerGlobalFilter.java
@@ -3,9 +3,10 @@ package com.kanitin.api_gateway.filter;
 import com.kanitin.api_gateway.config.RouterValidator;
 import com.kanitin.api_gateway.service.JwtService;
 import lombok.AllArgsConstructor;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -13,10 +14,16 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@RefreshScope
+import java.util.List;
+import java.util.UUID;
+
 @Component
+@Log4j2
 @AllArgsConstructor
-public class AuthenticationFilter implements GatewayFilter {
+public class CustomerGlobalFilter implements GlobalFilter {
+
+    private static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
+    private static final String CORRELATION_ID_LOG_VAR_NAME = "correlationId";
 
     private final RouterValidator routerValidator;
     private final JwtService jwtService;
@@ -24,19 +31,30 @@ public class AuthenticationFilter implements GatewayFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders requestHeaders = request.getHeaders();
+        List<String> correlationIdList = requestHeaders.get(CORRELATION_ID_HEADER_NAME);
+        String correlationId = UUID.randomUUID().toString();
+        if (correlationIdList != null && !correlationIdList.isEmpty()) {
+            correlationId = correlationIdList.getFirst();
+        }
+        exchange.getRequest().mutate().header(CORRELATION_ID_HEADER_NAME, correlationId);
+
+        log.info(CORRELATION_ID_LOG_VAR_NAME + ": " + correlationId);
+        log.info("METHOD: " + request.getMethod().name());
+        log.info("PATH: " + request.getPath());
 
         if (routerValidator.isSecured.test(request)) {
             if (this.isAuthMissing(request)) {
                 return this.onError(exchange, HttpStatus.UNAUTHORIZED);
             }
 
-            String token = this.getAuthHeader(request);
+            String token = this.getAuthHeader(request).replaceFirst("Bearer ", "");
 
-            if (jwtService.verifyJwtToken(token)) {
+            if (!jwtService.verifyJwtToken(token)) {
                 return this.onError(exchange, HttpStatus.FORBIDDEN);
             }
-
         }
+
         return chain.filter(exchange);
     }
 
@@ -53,5 +71,4 @@ public class AuthenticationFilter implements GatewayFilter {
     private boolean isAuthMissing(ServerHttpRequest request) {
         return !request.getHeaders().containsKey("Authorization");
     }
-
 }
